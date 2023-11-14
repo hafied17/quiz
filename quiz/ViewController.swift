793a75b36889ba853300476abf86333a833a1a52
hafied <hafiedmadrid6@gmail.com>
2023-11-14 16:53:05 +0700

    Initial Commit

---
+++ quiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  quiz
+//
+//  Created by Investo Medika Asia on 14/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
